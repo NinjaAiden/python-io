764de34fb867944deef2043e32ab3272fbf8ae2b
Aiden Harvey <aidenharvey102@gmail.com>
2019-01-13 09:23:29 +0000

    added a way to concatenate strings

--- write.py
+++ write.py
@@ -4,8 +4,11 @@ f = open("newfile.txt", "a")
 # create an array to write to file
 lines = ["Hello ", "World ", "Welcome ", "To ", "File IO"]
 
+# concatenate strings, in this case adding new line after every element in the array
+text = '\n'.join(lines)
+
 # write to file
-f.writelines(lines)
+f.writelines(text)
 
 # close file when finished
 f.close
