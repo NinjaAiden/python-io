6a828f95a25c395eca338ccf1acb88ce339d0daa
Aiden Harvey <aidenharvey102@gmail.com>
2019-01-13 08:47:26 +0000

    added commenting to code

--- quiz.py
+++ quiz.py
@@ -1,4 +1,8 @@
+# create a variable and assign a filepath to it
 f = open("data.txt", "r")
+# create a variable and assign contents of file to it
 lines = f.read()
+# close file when finished
 f.close()
+# print contents of file
 print(lines)
