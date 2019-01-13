0b66abe66e5d3831f572676489b46e053af3f5e4
Aiden Harvey <aidenharvey102@gmail.com>
2019-01-13 09:19:54 +0000

    write to file using an array

--- write.py
+++ write.py
@@ -1,8 +1,11 @@
 # open file in write mode
 f = open("newfile.txt", "a")
 
+# create an array to write to file
+lines = ["Hello ", "World ", "Welcome ", "To ", "File IO"]
+
 # write to file
-f.write(" world")
+f.writelines(lines)
 
 # close file when finished
 f.close
